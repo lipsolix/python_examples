0a5217c4701a1d15a04924000edc2538b5d7765d
Oleg Lihogub <oligli@yandex.ru>
2020-07-07 22:07:41 +0300

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,10 +1,23 @@
-from math import sqrt, log
+from math import log, sqrt
 from matplotlib import pyplot as plt
 
 f = lambda t, x: t**2*sqrt(x**2 + t**2)
-_F = lambda t, x: x*sqrt(t**2 + x**2)*(2*t**3+t*x**2)/abs(x)/8 - x**4*log(abs(x*sqrt(t**2+x**2)+t*abs(x)))/8
-F = lambda t, x: _F(t, x) if x != 0 else _F(t, 10E-20)
-delta = lambda pair: abs(pair[0] - pair[1])
+F = lambda t, x: x*sqrt(t**2 + x**2)*(2*t**3+t*x**2)/abs(x)/8 - x**4*log(abs(x*sqrt(t**2+x**2)+t*abs(x)))/8
+diff = lambda a: abs(a[1] - a[0])
+
+
+def CalcLimit(t, x):
+    epsilon = 10E-10
+    delta = 1
+    Y1 = F(t, x + delta)
+    delta /= 2
+    Y2 = F(t, x + delta)
+    while abs(Y1-Y2) > epsilon:
+        delta /= 2
+        Y1 = Y2
+        Y2 = F(t, x + delta)
+    return Y2
+
 
 a = float(input("Введите a: "))
 b = float(input("Введите b: "))
@@ -15,9 +28,11 @@ N = int(input("Введите N: "))
 X = [c + i * (d-c) / 20 for i in range(21)]
 h = (b-a) / N
 T = [a + i * h for i in range(N + 1)]
+
 Iapproximate = [h * sum([(f(T[i - 1], x) + f(T[i], x)) / 2 for i in range(1, N + 1)]) for x in X]
-Iaccurate = [F(b, x) - F(a, x) for x in X]
-print("Максимальная невязка: {}".format(max(list(map(delta, list(zip(Iapproximate, Iaccurate)))))))
+Iaccurate = [F(b, x) - F(a, x) if x != 0 else (CalcLimit(b, x) - CalcLimit(a, x)) for x in X]
+
+print("Максимальная невязка: {}".format(max(list(map(diff, list(zip(Iapproximate, Iaccurate)))))))
 
 plt.title("Графики значений определенного интеграла при X от {} до {}".format(c, d))
 plt.ylabel("Значение определенного интеграла")
@@ -25,5 +40,7 @@ plt.xlabel("Значение X")
 plt.grid(True)
 plt.plot(X, Iaccurate, color="green", label="Точное")
 plt.plot(X, Iapproximate, color="red", label="Приближенное")
+plt.xlim(min(X), max(X))
+plt.ylim(min(Iaccurate + Iapproximate), max(Iaccurate + Iapproximate))
 plt.legend()
 plt.show()
